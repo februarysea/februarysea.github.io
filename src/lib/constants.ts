76e7b682a20ec0a8ea2ed7bd935d7ed78ffbcfb9
februarysea <31513558+februarysea@users.noreply.github.com>
2026-01-15 00:04:44 +0800

    feat: update intro layout and content

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const LINKS = {
+  github: "https://github.com/februarysea",
+  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
+  medium: "https://ladvace.medium.com/",
+  discord: "https://discordapp.com/users/163300027618295808",
+  dribble: "https://dribbble.com/Ladvace_Jace",
+  email: "jichunhou2022@gmail.com",
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
